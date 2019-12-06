5d98cb36e31a0b514a30629aa729d4467963e509
od3ng <noprianto@polinema.ac.id>
2019-12-06 08:57:30 +0700

    commit pertama

---
+++ src/main/java/com/sinaungoding/ta/TaImporterApplication.java
@@ -0,0 +1,13 @@
+package com.sinaungoding.ta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaImporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaImporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sinaungoding/ta/TaImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinaungoding.ta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaImporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
